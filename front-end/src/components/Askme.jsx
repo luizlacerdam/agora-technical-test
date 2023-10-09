6844012ee7d1f024e9cd9d00e45edd0a109777e6
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-10-09 10:03:25 -0300

    :sparkles: [feat] pop-number

--- front-end/src/components/Askme.jsx
+++ front-end/src/components/Askme.jsx
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 export default function Askme({ question, setQuestion }) {
   const [input, setInput] = React.useState('');
   const MAX_LENGTH = 50;
+  const MAX_QUESTIONS = 3;
+
   const handleOnClick = () => {
     if (input === '' || input.length > MAX_LENGTH) return;
 
@@ -17,23 +19,34 @@ export default function Askme({ question, setQuestion }) {
           Pergunte-me
         </span>
       </div>
-      <div id="askme-input-div">
-        <input
-          type="text"
-          placeholder="Envie uma pergunta"
-          value={ input }
-          onChange={ (e) => setInput(e.target.value) }
-          maxLength={ MAX_LENGTH }
-        />
-
-      </div>
-      <div id="askme-button-div">
-        <button
-          onClick={ handleOnClick }
-        >
-          Enviar
-        </button>
-      </div>
+      { question.length === MAX_QUESTIONS && (
+        <div id="askme-pop">
+          <span id="pop-number">
+            {question.length}
+          </span>
+          <span>
+            Perguntas
+          </span>
+        </div>)}
+      { question.length < MAX_QUESTIONS && (
+        <>
+          <div id="askme-input-div">
+            <input
+              type="text"
+              placeholder="Envie uma pergunta"
+              value={ input }
+              onChange={ (e) => setInput(e.target.value) }
+              maxLength={ MAX_LENGTH }
+            />
+          </div>
+          <div id="askme-button-div">
+            <button
+              onClick={ handleOnClick }
+            >
+              Enviar
+            </button>
+          </div>
+        </>)}
     </div>
   );
 }
